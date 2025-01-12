5a978dde4f99fce382b889aa30cb220e2cbc8b1b
Maksymilian Piotrowicz <mpiotrowicz@soldevelo.com>
2025-01-12 14:26:10 +0100

    Added public field, refactored execution

--- lajter/action.py
+++ lajter/action.py
@@ -24,12 +24,21 @@ def get_by_id(id):
 
 
 def from_entry(entry):
-    return Action(
-        entry['type'],
-        entry['id'],
-        entry['value'],
-        entry['target']
-    )
+    try:
+        return Action(
+            entry['type'],
+            entry['id'],
+            entry['value'],
+            entry['target'],
+            entry['public']
+        )
+    except KeyError:
+        return Action(
+            entry['type'],
+            entry['id'],
+            entry['value'],
+            entry['target']
+        )
 
 class ActionType(Enum):
     SEND_MESSAGE = "send message"
@@ -48,11 +57,14 @@ class ActionType(Enum):
 class Action:
     db = TinyDB("actions.json")
 
-    def __init__(self, action_type, action_id=None, value=None, target=None):
+    def __init__(self, action_type: ActionType | str, action_id=None,
+                 value=None, target=None, public=False):
         self.id = action_id
 
         if type(action_type) is str:
             self.action_type: ActionType = ActionType(action_type)
+        else:
+            self.action_type = action_type
 
         if value is None:
             self.value = []
@@ -64,13 +76,19 @@ class Action:
         else:
             self.target = target
 
+        if type(public) is bool:
+            self.public = public
+        else:
+            self.public = bool(public)
+
     def save(self):
         if self.id is None:
             self.id = Action.db.insert({
                 'id': 'null',
                 'type': self.action_type.value,
                 'value': self.value,
-                'target': self.target
+                'target': self.target,
+                'public': self.public
             })
 
             Action.db.update({'id': self.id}, doc_ids=[self.id])
@@ -79,7 +97,8 @@ class Action:
                 'id': self.id,
                 'type': self.action_type.value,
                 'value': self.value,
-                'target': self.target
+                'target': self.target,
+                'public': self.public
             }, where('id') == self.id)
 
     def to_string(self) -> str:
@@ -155,178 +174,156 @@ class Action:
         if channel is None:
             channel = await lajter.utils.get_default_channel(bot)
 
-        match self.action_type:
-            case ActionType.SEND_MESSAGE:
-                try:
+        if db_user is None and member:
+            db_user = lajter.user.get_by_id(member.id)
+
+        try:
+            match self.action_type:
+                case ActionType.SEND_MESSAGE:
                     if self.target:
                         for target in self.target:
                             target_channel = await bot.fetch_channel(target)
                             await target_channel.send(self.value[0])
-                    if channel is not None:
+                    elif channel:
                         await channel.send(self.value[0])
-                except Exception:
-                    logger.warning(f'Failed to send a message: {traceback.format_exc()}')
-            case ActionType.DELETE_MESSAGE:
-                if message is not None:
-                    try:
+                case ActionType.DELETE_MESSAGE:
+                    if message:
                         if self.value:
                             await message.delete(delay=float(self.value[0]))
                         else:
                             await message.delete()
-                    except Exception:
-                        logger.warning(f'Failed to remove a message: {traceback.format_exc()}')
-            case ActionType.GIVE_ROLE:
-                if self.value:
-                    try:
+                case ActionType.GIVE_ROLE:
+                    if member and self.value:
                         role = role_from_mention(member.guild, self.value[0])
                         target = member
                         if self.target:
-                            target = await member_from_mention(member.guild, self.target[0])
+                            target = await member_from_mention(
+                                member.guild, self.target[0])
                         await target.add_roles(role)
-                    except Exception:
-                        logger.warning(f'Failed to add a role: {traceback.format_exc()}')
-            case ActionType.REMOVE_ROLE:
-                if self.value:
-                    try:
+                case ActionType.REMOVE_ROLE:
+                    if member and self.value:
                         role = role_from_mention(member.guild, self.value[0])
                         target = member
                         if self.target:
-                            target = await member_from_mention(member.guild, self.target[0])
+                            target = await member_from_mention(
+                                member.guild, self.target[0])
                         await target.remove_roles(role)
-                    except Exception:
-                        logger.warning(f'Failed to remove a role: {traceback.format_exc()}')
-            case ActionType.TIMEOUT:
-                if self.value:
-                    try:
+                case ActionType.TIMEOUT:
+                    if member and self.value:
+                        time = float(self.value[0])
                         target = member
                         if self.target:
-                            target = await member_from_mention(member.guild, self.target[0])
-                        await target.timeout(timedelta(seconds=float(self.value[0])))
-                    except Exception:
-                        logger.warning(f'Failed to timeout an user: {traceback.format_exc()}')
-            case ActionType.KICK:
-                try:
-                    target = member
-                    if self.target:
-                        target = await member_from_mention(member.guild, self.target[0])
-                    await target.kick()
-                except Exception:
-                    logger.warning(f'Failed to kick an user: {traceback.format_exc()}')
-            case ActionType.BAN:
-                try:
-                    target = member
-                    if self.target:
-                        target = await member_from_mention(member.guild, self.target[0])
-                    await target.ban(delete_message_seconds=0)
-                    lajter.user.User.db.remove(where('id') == db_user.id)
-                except Exception:
-                    logger.warning(f'Failed to ban an user: {traceback.format_exc()}')
-            case ActionType.CHANGE_NAME:
-                try:
-                    target = member
-                    if self.target:
-                        target = await member_from_mention(member.guild, self.target[0])
-                    if target.guild.owner is not target:
+                            target = await member_from_mention(
+                                member.guild, self.target[0])
+                        await target.timeout(timedelta(seconds=time))
+                case ActionType.KICK:
+                    if member:
+                        target = member
+                        if self.target:
+                            target = await member_from_mention(
+                                member.guild, self.target[0])
+                        await target.kick()
+                case ActionType.BAN:
+                    if member:
+                        target = member
+                        ban_role = await lajter.utils.get_ban_role(bot)
+                        if self.target:
+                            target = await member_from_mention(
+                                member.guild, self.target[0])
+                        await target.add_roles(ban_role)
+                        await channel.send(f'{member.mention} Bardzo się '
+                                           f'starałeś, ale z gry wyleciałeś. '
+                                           f'punkty: {db_user.points}')
+                        print(lajter.user.User.db.remove(where('id') == db_user.id))
+                        db_user = None
+                case ActionType.CHANGE_NAME:
+                    if member and self.value:
+                        target = member
+                        if self.target:
+                            target = await member_from_mention(
+                                member.guild, self.target[0])
                         await target.edit(nick=self.value[0])
-                except Exception:
-                    logger.warning(f'Failed to change name: {traceback.format_exc()}')
-            case ActionType.ADD_POINTS:
-                try:
-                    target = db_user
-                    if self.target:
-                        target = await member_from_mention(member.guild, self.target[0])
-                        target = lajter.user.get_by_id(target.id)
+                case ActionType.ADD_POINTS:
+                    if db_user and self.value:
+                        target = db_user
+                        if self.target:
+                            target = await member_from_mention(
+                                member.guild, self.target[0])
+                            target = lajter.user.get_by_id(target.id)
+                        target.points += int(self.value[0])
+                case ActionType.POLL:
+                    if member and channel:
+                        target = member
+                        target_channel = channel
 
-                    target.points += int(self.value[0])
-                    target.save()
-                except Exception:
-                    logger.warning(
-                        f'Failed to change name: {traceback.format_exc()}')
-            case ActionType.POLL:
-                try:
-                    target = member
-                    target_channel = channel
+                        if self.target:
+                            target_channel = await bot.fetch_channel(
+                                self.target[0][2:-1])
 
-                    if self.target:
-                        target_channel = await bot.fetch_channel(
-                            self.target[0][2:-1])
+                        if len(self.target) > 1:
+                            target = await member_from_mention(
+                                member.guild,self.target[1])
 
-                    if len(self.target) > 1:
-                        target = await member_from_mention(member.guild,
-                                                           self.target[1])
+                        timeout = timedelta(minutes=5)
+                        action_to_execute = None
 
-                    timeout = timedelta(minutes=5)
-                    action_to_execute = None
+                        if self.value:
+                            action_to_execute = get_by_id(int(self.value[0]))
+                        if len(self.value) > 1:
+                            timeout = timedelta(seconds=int(self.value[1]))
 
-                    if self.value:
-                        action_to_execute = get_by_id(int(self.value[0]))
-                    if len(self.value) > 1:
-                        timeout = timedelta(seconds=int(self.value[1]))
-
-                    s = f'Rozpoczęto głosowanie przeciwko {target.mention}.\nPotrzeba powyżej 50% głosów'
-                    if action_to_execute:
-                        s += f', żeby wykonać akcję: {action_to_execute.to_string()}'
-                    s += ".\n"
-                    s += f' Głosowanie potrwa: `{timeout}`'
-
-                    poll = await target_channel.send(s)
-                    await poll.add_reaction("👍")
-                    await poll.add_reaction("👎")
-
-                    await asyncio.sleep(timeout.seconds)
-
-                    poll = await target_channel.fetch_message(poll.id)
-
-                    result = 0
-
-                    for reaction in poll.reactions:
-                        if reaction.emoji == "👍":
-                            result += reaction.count
-                        elif reaction.emoji == "👎":
-                            result -= reaction.count
-
-                    if result > 0:
-                        await poll.reply(f'Głosowanie przeciwko {target.mention} przeszło większością głosów.')
+                        s = "Rozpoczęto głosowanie przeciwko "
+                        s += target.mention
+                        s += ".\nPotrzeba powyżej 50% głosów"
                         if action_to_execute:
-                            if target != member:
-                                db_user = lajter.user.get_by_id(target.id)
-                            await action_to_execute.execute(bot, target, db_user, channel)
-                    else:
-                        await poll.reply(f'Głosowanie przeciwko {target.mention} nie uzyskało większości głosów.')
-                except Exception:
-                    logger.warning(
-                        f'Failed to make a poll: {traceback.format_exc()}')
-            case ActionType.RANDOM:
-                try:
-                    target = member
-                    target_channel = channel
+                            s += (f', żeby wykonać akcję: '
+                                  f'{action_to_execute.to_string()}')
+                        s += ".\n"
+                        s += f' Głosowanie potrwa: `{timeout}`'
 
-                    if self.target:
-                        target_channel = await bot.fetch_channel(
-                            self.target[0][2:-1])
+                        poll = await target_channel.send(s)
+                        await poll.add_reaction("👍")
+                        await poll.add_reaction("👎")
 
+                        await asyncio.sleep(timeout.seconds)
+
+                        poll = await target_channel.fetch_message(poll.id)
+
+                        result = 0
+
+                        for reaction in poll.reactions:
+                            if reaction.emoji == "👍":
+                                result += reaction.count
+                            elif reaction.emoji == "👎":
+                                result -= reaction.count
+
+                        if result > 0:
+                            await poll.reply(f'Głosowanie przeciwko '
+                                             f'{target.mention} przeszło '
+                                             f'większością głosów.')
+                            if action_to_execute:
+                                if target != member:
+                                    db_user = lajter.user.get_by_id(target.id)
+                                await action_to_execute.execute(
+                                    bot, target, db_user, channel)
+                        else:
+                            await poll.reply(f'Głosowanie przeciwko '
+                                             f'{target.mention} nie uzyskało '
+                                             f'większości głosów.')
+                case ActionType.RANDOM:
                     if self.value:
                         random_action = random.choice(self.value)
                         random_action = get_by_id(int(random_action))
-                        await random_action.execute(bot, target, db_user, target_channel, message)
-                except Exception:
-                    logger.warning(
-                        f'Failed to make random choice: {traceback.format_exc()}')
-            case ActionType.CHAIN:
-                try:
-                    target = member
-                    target_channel = channel
+                        await random_action.execute(bot, member, db_user,
+                                                    channel, message)
+                case ActionType.CHAIN:
+                    if self.value:
+                        for action_id in self.value:
+                            action = get_by_id(int(action_id))
+                            await action.execute(bot, member, db_user,
+                                                 channel,message)
 
-                    if self.target:
-                        target_channel = await bot.fetch_channel(
-                            self.target[0][2:-1])
-
-                    for action_id in self.value:
-                        action = get_by_id(int(action_id))
-                        await action.execute(bot, member, db_user, channel,message)
-                except Exception:
-                    logger.warning(
-                        f'Failed to execute action chain: {traceback.format_exc()}')
-
+        except Exception:
+            logger.error(f'Failed to execute action: {traceback.format_exc()}')
         if db_user:
             db_user.save()
