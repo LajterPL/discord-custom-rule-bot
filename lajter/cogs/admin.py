a81fc1dff55c46d31cb40292df5f98d80ada76ba
Maksymilian Piotrowicz <mpiotrowicz@soldevelo.com>
2025-01-20 15:05:59 +0100

    Added addpoints command

--- lajter/cogs/admin.py
+++ lajter/cogs/admin.py
@@ -1,5 +1,6 @@
 import logging
 
+import lajter.user
 from lajter.action import Action, ActionType
 from discord.ext import commands
 from discord import Member
@@ -20,3 +21,11 @@ class Admin(commands.Cog):
     async def ban(self, ctx: commands.Context, member: Member):
         action = Action(ActionType.BAN)
         await action.execute(self.bot, member)
+
+
+    @commands.command(name="addpoints")
+    @commands.has_guild_permissions(administrator=True)
+    async def admin_add_points(self, ctx: commands.Context, member: Member, amount: int):
+        db_user = lajter.user.get_by_id(member.id)
+        db_user.points += amount
+        db_user.save()
